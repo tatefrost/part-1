67ef829cf69ed9e776d98510e352895c1d2ffa86
Tate Frost <tatefrost96@gmail.com>
2021-12-02 15:46:59 -0700

    initial commit

---
+++ cipher.js
@@ -0,0 +1,10 @@
+alphabet = ["w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+list = 'EHKRAZNULKCNWLDV'
+const decipher = code => {
+  code.split('')
+  code.toLowerCase()
+  for (let i = 0; i < code.length; i++){
+    let let = code[i]
+  }
+}
