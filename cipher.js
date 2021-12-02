2f8df6a1d2166abd3e115ba58f6deda128449331
Tate Frost <tatefrost96@gmail.com>
2021-12-02 16:39:57 -0700

    second commit

--- cipher.js
+++ cipher.js
@@ -1,10 +1,19 @@
-alphabet = ["w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+//program not working
 
+alphabet = ["w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+let decoded = []
 list = 'EHKRAZNULKCNWLDV'
 const decipher = code => {
-  code.split('')
-  code.toLowerCase()
+  code = code.toLowerCase()
+  code = code.split('')
+  console.log(code)
   for (let i = 0; i < code.length; i++){
-    let let = code[i]
+    let found = alphabet.find(el => code[i] === el)
+    console.log(found)
+    decoded.push(found)
   }
+    decoded = decoded.join('')
+    return decoded
 }
+
+console.log(decipher(list));
